9eefc7c86d231e88dd2857d6bb4c3edf0963a46c
Simon <gevhaz@tutanota.com>
2022-07-01 18:04:46 +0200

    Check upcoming birtdays
    Check if a birthday is coming up, and is so, print a message.

--- birthdaysay/app.py
+++ birthdaysay/app.py
@@ -1,12 +1,18 @@
 import sys
-import pprint
+from datetime import date, datetime
+from typing import TypedDict
 
 from vobject.base import ParseError, readComponents
 
 from birthdaysay.cli import Cli
 
 
-def get_contacts(contacts_file: str) -> list[dict[str, str | None]]:
+class Person(TypedDict):
+    name: str
+    birthday: date
+
+
+def get_contacts(contacts_file: str) -> list[Person]:
     try:
         with open(contacts_file, "r") as f:
             contacts_data = f.read()
@@ -14,7 +20,7 @@ def get_contacts(contacts_file: str) -> list[dict[str, str | None]]:
         print(f"The vcard file {contacts_file} doesn't seem to exist")
         return []
 
-    all_contacts: list[dict[str, str | None]] = []
+    all_contacts: list[Person] = []
 
     contact_components = readComponents(contacts_data)
 
@@ -41,9 +47,10 @@ def get_contacts(contacts_file: str) -> list[dict[str, str | None]]:
             name = name.strip().replace("  ", " ")
             name = name.replace("\u3000", " ")  # This character showed up in a contact
             birthday = birthday.strip().replace("  ", " ")
+            parsed_birtday = datetime.strptime(birthday, "%Y%m%d").date()
 
             if birthday is not None:
-                all_contacts.append({"name": name, "birthday": birthday})
+                all_contacts.append({"name": name, "birthday": parsed_birtday})
 
             contact = next(contact_components, None)
     except ParseError as e:
@@ -59,7 +66,20 @@ def main() -> int:
 
     contacts = get_contacts(args.contacts_file)
 
-    pprint.pprint(contacts)
+    for c in contacts:
+        next_birthday = date(date.today().year, c["birthday"].month, c["birthday"].day)
+        if next_birthday < date.today():
+            next_birthday = next_birthday.replace(year=date.today().year + 1)
+
+        if next_birthday == date.today():
+            print(f"It's {c['name']}'s birtday today!")
+
+        elif (next_birthday - date.today()).days == 1:
+            print("It's {c['name']}'s birthday tomorrow")
+
+        elif (next_birthday - date.today()).days in [6, 7]:
+            print_birtday = date.strftime(next_birthday, "%B %d")
+            print(f"It's {c['name']}'s birthday on {print_birtday}")
 
     return 0
 
